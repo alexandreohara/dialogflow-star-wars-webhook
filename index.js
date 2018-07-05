3f78e2b421d55bd565e55515a7c3789c8c8c6041
alexandreohara <alexandre.ohara@hotmail.com>
2018-07-05 09:20:37 -0300

    get height from other characters

--- index.js
+++ index.js
@@ -15,19 +15,30 @@ server.use(bodyParser.json());
 
 
 
-request('https://swapi.co/api/people/1', function (error, response, body) {
+request('https://swapi.co/api/people/', function (error, response, body) {
     var parsedData = JSON.parse(body);
+    for (var index in parsedData['results']) {
+        console.log(parsedData['results'][index]['name']);
+    }
     server.post("/people", function(req, res) {  
-        var person =
-          req.body.queryResult &&
-          req.body.queryResult.parameters &&
-          req.body.queryResult.parameters.people
-            ? req.body.queryResult.parameters.people
-            : "Unknown";
-        return res.json({
-          fulfillmentText: person + "'s height is " + parsedData['height'] + "cm",
-          source: "person height"
-        });
+        var person = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.people ? req.body.queryResult.parameters.people : "Unknown";
+        for (var index in parsedData['results']) {
+            var characterName = parsedData['results'][index]['name'];
+            if (person == characterName) {
+                return res.json({
+                    fulfillmentText: person + "'s height is " + parsedData['height'] + "cm",
+                    source: "person height"
+                });
+            }
+            else {
+                return res.json({
+                    fulfillmentText: "Something went wrong",
+                    source: "person height"
+                });
+            }
+        }
+
+
       });
 });
 
