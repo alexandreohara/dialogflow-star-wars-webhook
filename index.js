45c22b09f4df78693ce58bff4f905d2fb42df7f1
alexandreohara <alexandre.ohara@hotmail.com>
2018-07-05 03:18:52 -0300

    update response params

--- index.js
+++ index.js
@@ -19,14 +19,13 @@ request('https://swapi.co/api/people/1', function (error, response, body) {
     var parsedData = JSON.parse(body);
     server.post("/people", function(req, res) {  
         var person =
-          req.body.result &&
-          req.body.result.parameters &&
-          req.body.result.parameters.people
-            ? req.body.result.parameters.people
+          req.body.queryResult &&
+          req.body.queryResult.parameters &&
+          req.body.queryResult.parameters.people
+            ? req.body.queryResult.parameters.people
             : "Unknown";
         return res.json({
-          speech: person + "'s height is " + parsedData,
-          displayText: person + "'s height is " + parsedData,
+          fulfillmentText: person + "'s height is " + parsedData,
           source: "person height"
         });
       });
